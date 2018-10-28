f0d54ba44ed2fe1c0492f5dbdec384c74d1ce5e5
Shelton Suen <sheltonsuen@gmail.com>
2018-10-28 22:58:11 +0800

    [Shelton] - Move the previous work to this project.

---
+++ src/README.md
@@ -0,0 +1,21 @@
+# Refactoring Check List
+
+## Composing Method
+
+[x] Inline Temp
+[x] Remove Assignments to Parameters
+[x] Replace Temp with Query
+[x] Split Temporay Variable
+[x] Substitute Algorithm
+
+## Simplifying Conditional Expressions
+
+[x] Consolidate Conditional Expression
+[x] Consolidate Duplicated Conditional Fragments
+[x] Decompose Conditional
+[x] Remove Control Flag
+[x] Replace Conditional with Guard Clauses
+
+## Simplifying Method Calls
+
+[x] Add Parameter
---
+++ src/simplifyingMethodCalls/__test__/addParameterSpecs.js
@@ -0,0 +1,15 @@
+import { person, afterRefactoring_person } from '../addParameter';
+
+describe('addParameter', () => {
+  it('should return the words that the person want to say', () => {
+    person.words = 'some thing need to say';
+
+    expect(person.say()).to.be.equal('some thing need to say');
+  });
+
+  it('should return the words that the person want to say', () => {
+    expect(afterRefactoring_person.say('some thing need to say')).to.be.equal(
+      'some thing need to say'
+    );
+  });
+});
