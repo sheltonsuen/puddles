bf5ce91fad34533f73cc8d154c6e7e1895c82a50
Shelton Suen <sheltonsuen@gmail.com>
2018-10-28 22:35:43 +0800

    [Shelton] - Add mocha, chai and sinon to support testing.

---
+++ test/detectorSpecs.js
@@ -0,0 +1,17 @@
+import sinon from "sinon";
+
+const sandbox = sinon.createSandbox();
+
+describe("Detector", () => {
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("should working with sinon", () => {
+    const fooStub = sandbox.stub();
+
+    fooStub();
+
+    expect(fooStub).to.be.called;
+  });
+});
